e1015f4d9e074a3cf7455cb06b087e7285454b4b
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2025-06-24 00:18:04 +0200

    added material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
