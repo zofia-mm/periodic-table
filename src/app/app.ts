16eb0b94aa583d268a744faa47115a6231335f56
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2025-06-24 01:20:32 +0200

    layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet,
+    MatSlideToggleModule, MatToolbarModule, MatInputModule,
+    MatIconModule, MatTableModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'periodic-table';
+  displayedColumns: String[] = ["position"];
+  dataSource: {position: number}[] = [{ position: 1 }, {position: 2}]
 }
