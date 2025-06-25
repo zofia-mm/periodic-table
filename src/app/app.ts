6ef85451a8e9d4a3da7f7885e3c7f74237b70146
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2025-06-25 13:14:20 +0200

    signal store

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,23 +4,21 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { ElementService } from './element.service';
+import { ElementService } from './elements-table/element.service';
+import { ElementsTable } from "./elements-table/elements-table";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
     MatSlideToggleModule, MatToolbarModule, MatInputModule,
-    MatIconModule, MatTableModule],
+    MatIconModule, ElementsTable],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   protected title = 'periodic-table';
-  displayedColumns: String[] = ["position"];
-  dataSource: {position: number}[] = [{ position: 1 }, {position: 2}]
-  elementService: ElementService = inject(ElementService);
 
+  elementService: ElementService = inject(ElementService);
   ngOnInit(): void {
     this.elementService.getElements().forEach( console.log );
   }
