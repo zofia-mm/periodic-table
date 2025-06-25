24a86c294d31181571de3140417b2d63155a567d
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2025-06-25 22:37:00 +0200

    code cleanup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,21 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { ElementService } from './elements-table/element.service';
 import { ElementsTable } from "./elements-table/elements-table";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
+  imports: [ElementsTable,
     MatSlideToggleModule, MatToolbarModule, MatInputModule,
-    MatIconModule, ElementsTable],
+    MatIconModule, MatFormFieldModule, FormsModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'periodic-table';
-
   elementService: ElementService = inject(ElementService);
 }
